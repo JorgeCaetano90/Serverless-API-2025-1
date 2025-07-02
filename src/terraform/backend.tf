87d26fae31177176a4b313dc084eaaf901259a14
JorgeCaetano90 <112181487+JorgeCaetano90@users.noreply.github.com>
2025-07-02 20:13:57 -0300

    bkp3

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-aws-academy-2025-${var.student_id}"
+    bucket = "terraform-state-aws-academy-2025-jorge-caetano"
     key    = "academy-lab/terraform.tfstate"
     region = "us-east-1"
   }
