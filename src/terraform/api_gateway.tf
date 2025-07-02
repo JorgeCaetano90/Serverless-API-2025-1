05800ecaacd99bf4b5b87c3a3fb97413bbc49854
JorgeCaetano90 <112181487+JorgeCaetano90@users.noreply.github.com>
2025-07-02 20:48:58 -0300

    bkp5

--- src/terraform/api_gateway.tf
+++ src/terraform/api_gateway.tf
@@ -45,7 +45,7 @@ resource "aws_apigatewayv2_route" "lambda_route_jc_get" {
 
 resource "aws_apigatewayv2_route" "lambda_route_jc_post" {
   api_id    = aws_apigatewayv2_api.main_api.id
-  route_key = "POST /jc/users"
+  route_key = "POST /jc"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
